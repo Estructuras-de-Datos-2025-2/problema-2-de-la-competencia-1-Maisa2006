114354817d4c620d3bed9a8f1a5c28670820ec90
Maisa2006 <mariaisabelsolis386@gmail.com>
2025-08-29 10:05:04 -0500

    Update main.c
    prueba

--- main.c
+++ main.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
-int main(void)
-{
+
+int main() {
     int n;
-    scanf("%d",&n);
-    // TODO: malloc n, leer arreglo
-    // TODO: imprimir en orden inverso separados por espacio
-    // TODO: free
+    int i;
+    
+
+    scanf("%d", &n);
+    int arreglo[n];
+
+    for (i = 0; i< n; i++)
+        scanf("%d", &arreglo[i]);
+    
+    for(i=n-1;i>=0; i--)
+        printf("%d " "", arreglo[i]);
+
     return 0;
+    
 }
